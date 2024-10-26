49467fdece27b7c309e663a6f49e7bf86216474c
Minai Project <128550217+minaiproject@users.noreply.github.com>
2024-10-26 23:26:56 +0100

    Create config.py

---
+++ utils/config.py
@@ -0,0 +1,5 @@
+class Config:
+    SOLANA_RPC_URL = "https://api.mainnet-beta.solana.com"
+    CONTRACT_ADDRESS = "YourContractAddressHere"
+    DATA_DIR = "data/"
+    MODEL_SAVE_PATH = "models/agent_model.pth"
